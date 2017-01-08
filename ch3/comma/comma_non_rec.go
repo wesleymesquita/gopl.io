5d3639f31b305a2e5421a947be57117fb1b192cc
Wesley Mesquita <wesley@skynet.(none)>
2017-01-08 12:28:54 -0200

    Comman non rec

---
+++ ch3/comma/comma_non_rec.go
@@ -0,0 +1,52 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 73.
+
+// Comma prints its argument numbers with a comma at each power of 1000.
+//
+// Example:
+// 	$ go build gopl.io/ch3/comma
+//	$ ./comma 1 12 123 1234 1234567890
+// 	1
+// 	12
+// 	123
+// 	1,234
+// 	1,234,567,890
+//
+package main
+
+import (
+	"fmt"
+	"os"
+	"bytes"
+)
+
+func main() {
+	for i := 1; i < len(os.Args); i++ {
+		fmt.Printf("  %s\n", comma(os.Args[i]))
+	}
+}
+
+//!+
+// comma inserts commas in a non-negative decimal integer string.
+func comma(s string) string {
+	n := len(s)
+	if n <= 3 {
+		return s
+	}
+	var buf bytes.Buffer
+	var k int = 0
+	for _,v:= range s {
+		if k == 3 {
+			buf.WriteString(",")
+			k = 1
+		} else {
+			k++
+		}	
+		buf.WriteRune(v)
+	}	
+	return buf.String()
+}
+
+//!-
